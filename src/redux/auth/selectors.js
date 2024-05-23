dfb518c8daf9284cec77e27b531179932ac153e7
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-05-23 22:50:30 +0300

    minor fixes

---
+++ src/redux/auth/selectors.js
@@ -0,0 +1,4 @@
+export const selectUserName = (state) => state.auth.user.name;
+export const selectToken = (state) => state.auth.token;
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+export const selectIsRefreshing = (state) => state.auth.isRefreshing;
